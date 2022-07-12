ceb82a5811f7a2f82a34478d5ad6e4eb6cbcea3a
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-07-12 13:47:55 -0300

    Use same version for localstack docker image as Python package

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,11 @@
+import os
 import shlex
 import subprocess
 
 import pytest
 
 from exasol_script_languages_developer_sandbox.lib.render_template import render_template
+from importlib.metadata import version
 
 
 @pytest.fixture
@@ -18,14 +20,19 @@ def local_stack():
     """
     command = "localstack start -d"
 
-    process = subprocess.run(shlex.split(command))
+    # We set the specific version for the docker image for localstack to use ("IMAGE_NAME"),
+    # otherwise localstack uses tag "latest" which might break the CI tests.
+    image_name = {"IMAGE_NAME": f"localstack/localstack:{version('localstack')}"}
+    env_variables = {**os.environ, **image_name}
+
+    process = subprocess.run(shlex.split(command), env=env_variables)
     assert process.returncode == 0
 
     command = "localstack wait -t 30"
 
-    process = subprocess.run(shlex.split(command))
+    process = subprocess.run(shlex.split(command), env=env_variables)
     assert process.returncode == 0
     yield None
 
     command = "localstack stop"
-    subprocess.run(shlex.split(command))
+    subprocess.run(shlex.split(command), env=env_variables)
