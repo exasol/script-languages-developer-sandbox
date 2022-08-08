9e712eab205863667161625d8c400123d9b47f75
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-08-08 15:09:23 -0300

    Commented tests

--- test/test_deploy_vm_bucket.py
+++ test/test_deploy_vm_bucket.py
@@ -18,6 +18,9 @@ def test_deploy_vm_bucket_template_with_cnf_lint(tmp_path, vm_bucket_cloudformat
 
 
 def test_find_bucket_with_mock():
+    """
+    This test uses a mock to validate the correct finding of the bucket in the stack.
+    """
     aws_access_mock = MagicMock()
     run_setup_vm_bucket(aws_access_mock)
     assert aws_access_mock.upload_cloudformation_stack.called
@@ -37,6 +40,9 @@ def test_find_bucket_with_mock():
 
 
 def test_find_fails_with_mock():
+    """
+    This test uses a mock to validate the raising of a RuntimeError exception if the bucket was not deployed.
+    """
     aws_access_mock = MagicMock()
     run_setup_vm_bucket(aws_access_mock)
     assert aws_access_mock.upload_cloudformation_stack.called
@@ -56,6 +62,9 @@ def test_find_fails_with_mock():
 
 
 def test_find_bucket(local_stack):
+    """
+    This test uses localstack to deploy the VM Bucket and then find it again.
+    """
     aws_access = AwsLocalStackAccess(None)
     run_setup_vm_bucket(aws_access)
     bucket = find_vm_bucket(aws_access)
