ff32f2f888a5bbe52f37bb404e8ac3c5f6961804
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-08-24 13:45:01 -0300

    #22: Improved logging

--- exasol_script_languages_developer_sandbox/lib/aws_access/aws_access.py
+++ exasol_script_languages_developer_sandbox/lib/aws_access/aws_access.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Optional, Any, List
 
 import boto3
@@ -14,6 +13,7 @@ from exasol_script_languages_developer_sandbox.lib.aws_access.key_pair import Ke
 from exasol_script_languages_developer_sandbox.lib.aws_access.s3_object import S3Object
 from exasol_script_languages_developer_sandbox.lib.aws_access.snapshot import Snapshot
 from exasol_script_languages_developer_sandbox.lib.aws_access.stack_resource import StackResource
+from exasol_script_languages_developer_sandbox.lib.logging import get_status_logger, LogType
 from exasol_script_languages_developer_sandbox.lib.tags import create_default_asset_tag
 from exasol_script_languages_developer_sandbox.lib.export_vm.vm_disk_image_format import VmDiskImageFormat
 
@@ -21,6 +21,7 @@ from exasol_script_languages_developer_sandbox.lib.export_vm.vm_disk_image_forma
 class AwsAccess(object):
     def __init__(self, aws_profile: Optional[str]):
         self._aws_profile = aws_profile
+        self._logger = get_status_logger(LogType.AWS_ACCESS)
 
     @property
     def aws_profile_for_logging(self) -> str:
@@ -37,7 +38,7 @@ class AwsAccess(object):
         """
         Create an EC-2 Key-Pair, identified by parameter 'key_name'
         """
-        logging.debug(f"Running create_new_ec2_key_pair for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running create_new_ec2_key_pair for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         tags = [{"ResourceType": "key-pair", "Tags": create_default_asset_tag(tag_value)}]
         key_pair = cloud_client.create_key_pair(KeyName=key_name, TagSpecifications=tags)
@@ -47,7 +48,7 @@ class AwsAccess(object):
         """
         Delete the EC-2 Key-Pair, given by parameter 'key_name'
         """
-        logging.debug(f"Running delete_ec2_key_pair for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running delete_ec2_key_pair for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         cloud_client.delete_key_pair(KeyName=key_name)
 
@@ -55,7 +56,7 @@ class AwsAccess(object):
         """
         Deploy the cloudformation stack.
         """
-        logging.debug(f"Running upload_cloudformation_stack for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running upload_cloudformation_stack for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("cloudformation")
         try:
             cfn_deployer = Deployer(cloudformation_client=cloud_client)
@@ -64,14 +65,14 @@ class AwsAccess(object):
                                                                 capabilities=("CAPABILITY_IAM",), role_arn=None,
                                                                 notification_arns=None, tags=tags)
         except Exception as e:
-            logging.error(f"Error creating changeset for cloud formation template: {e}")
+            self._logger.error(f"Error creating changeset for cloud formation template: {e}")
             raise e
         try:
             cfn_deployer.execute_changeset(changeset_id=result.changeset_id, stack_name=stack_name)
             cfn_deployer.wait_for_execute(stack_name=stack_name, changeset_type=result.changeset_type)
         except Exception as e:
-            logging.error(f"Error executing changeset for cloud formation template: {e}")
-            logging.error(f"Run 'aws cloudformation describe-stack-events --stack-name {stack_name}' to get details.")
+            self._logger.error(f"Error executing changeset for cloud formation template: {e}")
+            self._logger.error(f"Run 'aws cloudformation describe-stack-events --stack-name {stack_name}' to get details.")
             raise e
 
     def validate_cloudformation_template(self, cloudformation_yml) -> None:
@@ -81,7 +82,7 @@ class AwsAccess(object):
         Pitfall: Boto3 expects the YAML string as parameter, whereas the AWS CLI expects the file URL as parameter.
         It requires to have the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables set correctly.
         """
-        logging.debug(f"Running validate_cloudformation_template for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running validate_cloudformation_template for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("cloudformation")
         cloud_client.validate_template(TemplateBody=cloudformation_yml)
 
@@ -102,14 +103,14 @@ class AwsAccess(object):
         The AWS API truncates at a size of 1MB, and in order to get all chunks the method must be called
         passing the previous retrieved token until no token is returned.
         """
-        logging.debug(f"Running get_all_codebuild_projects for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running get_all_codebuild_projects for aws profile {self.aws_profile_for_logging}")
         return self._get_stack_resources(stack_name=stack_name)
 
     def stack_exists(self, stack_name: str) -> bool:
         """
         This functions uses Boto3 to check if stack with name `stack_name` exists.
         """
-        logging.debug(f"Running stack_exists for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running stack_exists for aws profile {self.aws_profile_for_logging}")
         try:
             result = self._get_stack_resources(stack_name=stack_name)
             return any([res.status != "DELETE_COMPLETE" for res in result])
@@ -120,7 +121,7 @@ class AwsAccess(object):
         """
         This functions uses Boto3 to delete a stack identified by parameter "stack_name".
         """
-        logging.debug(f"Running delete_stack for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running delete_stack for aws profile {self.aws_profile_for_logging}")
         cf_client = self._get_aws_client('cloudformation')
         cf_client.delete_stack(StackName=stack_name)
 
@@ -128,7 +129,7 @@ class AwsAccess(object):
         """
         This functions uses Boto3 to describe all cloudformation stacks.
         """
-        logging.debug(f"Running describe_stacks for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running describe_stacks for aws profile {self.aws_profile_for_logging}")
         cf_client = self._get_aws_client('cloudformation')
         current_result = cf_client.describe_stacks()
         result = current_result["Stacks"]
@@ -142,7 +143,7 @@ class AwsAccess(object):
         """
         Describes an AWS instance identified by parameter instance_id
         """
-        logging.debug(f"Running describe_instance for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running describe_instance for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         instances_result = cloud_client.describe_instances(InstanceIds=[instance_id])
         reservations = instances_result["Reservations"]
@@ -158,7 +159,7 @@ class AwsAccess(object):
         Creates an AMI image from an EC-2 instance.
         Returns the image-id of the new AMI.
         """
-        logging.debug(f"Running create_image_from_ec2_instance for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running create_image_from_ec2_instance for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         tags = [{"ResourceType": "image", "Tags": create_default_asset_tag(tag_value)},
                 {"ResourceType": "snapshot", "Tags": create_default_asset_tag(tag_value)}]
@@ -174,7 +175,7 @@ class AwsAccess(object):
         Creates an AMI image from an EC-2 instance.
         Returns the export_image_task_id.
         """
-        logging.debug(f"Running create_image_from_ec2_instance for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running create_image_from_ec2_instance for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         tags = [{"ResourceType": "export-image-task", "Tags": create_default_asset_tag(tag_value)}]
         result = cloud_client.export_image(ImageId=image_id, Description=description,
@@ -188,7 +189,7 @@ class AwsAccess(object):
         """
         Get Export-Image-Task for given export_image_task_id.
         """
-        logging.debug(f"Running create_image_from_ec2_instance for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running create_image_from_ec2_instance for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         result = cloud_client.describe_export_image_tasks(ExportImageTaskIds=[export_image_task_id])
         assert "NextToken" not in result  # We expect only one result
@@ -202,7 +203,7 @@ class AwsAccess(object):
         """
         Get AMI image for given image_id
         """
-        logging.debug(f"Running get_ami for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running get_ami for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
 
         response = cloud_client.describe_images(ImageIds=[image_id])
@@ -215,7 +216,7 @@ class AwsAccess(object):
         """
         Get EC-2 instance status for given instance_id
         """
-        logging.debug(f"Running get_instance_status for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running get_instance_status for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
 
         response = cloud_client.describe_instance_status(InstanceIds=[instance_id])
@@ -229,7 +230,7 @@ class AwsAccess(object):
         """
         List AMI images with given tag filter
         """
-        logging.debug(f"Running list_amis for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running list_amis for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         response = cloud_client.describe_images(Filters=filters)
         return [Ami(ami) for ami in response["Images"]]
@@ -238,7 +239,7 @@ class AwsAccess(object):
         """
         List EC2 volume snapthos with given tag filter
         """
-        logging.debug(f"Running list_snapshots for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running list_snapshots for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
 
         response = cloud_client.describe_snapshots(Filters=filters)
@@ -249,7 +250,7 @@ class AwsAccess(object):
         """
         List export image tasks with given tag filter
         """
-        logging.debug(f"Running list_export_image_tasks for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running list_export_image_tasks for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
 
         response = cloud_client.describe_export_image_tasks(Filters=filters)
@@ -260,7 +261,7 @@ class AwsAccess(object):
         """
         List ec-2 key-pairs with given tag filter
         """
-        logging.debug(f"Running list_ec2_key_pairs for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running list_ec2_key_pairs for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
 
         response = cloud_client.describe_key_pairs(Filters=filters)
@@ -271,7 +272,7 @@ class AwsAccess(object):
         """
         List s3 objects images with given tag filter
         """
-        logging.debug(f"Running list_s3_objects for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running list_s3_objects for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("s3")
 
         response = cloud_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
@@ -283,7 +284,7 @@ class AwsAccess(object):
         Get location (region) of s3 bucket
         Wraps: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.get_bucket_location
         """
-        logging.debug(f"Running get_s3_bucket_location for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running get_s3_bucket_location for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("s3")
 
         response = cloud_client.get_bucket_location(Bucket=bucket)
@@ -294,7 +295,7 @@ class AwsAccess(object):
         """
         De-registers an AMI
         """
-        logging.debug(f"Running deregister_ami for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running deregister_ami for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         cloud_client.deregister_image(ImageId=ami_d)
 
@@ -302,7 +303,7 @@ class AwsAccess(object):
         """
         Removes a snapshot
         """
-        logging.debug(f"Running remove_snapshot for aws profile {self.aws_profile_for_logging}")
+        self._logger.debug(f"Running remove_snapshot for aws profile {self.aws_profile_for_logging}")
         cloud_client = self._get_aws_client("ec2")
         cloud_client.delete_snapshot(SnapshotId=snapshot_id)
 
