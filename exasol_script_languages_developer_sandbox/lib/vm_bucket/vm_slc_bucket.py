4ed4cf8feea42d4d240b589975ccb929f9687c7b
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-09-22 08:48:05 -0300

    fixes from review

--- exasol_script_languages_developer_sandbox/lib/vm_bucket/vm_slc_bucket.py
+++ exasol_script_languages_developer_sandbox/lib/vm_bucket/vm_slc_bucket.py
@@ -19,12 +19,12 @@ class OutputKey(Enum):
 LOG = get_status_logger(LogType.VM_BUCKET)
 
 
-def create_vm_bucket_cf_template(acl_arn: str) -> str:
+def create_vm_bucket_cf_template(waf_webacl_arn: str) -> str:
     # All output keys (class OutputKey) are parameters in the vm_bucket_cloudformation.jinja.yaml
     # Simply map the output key enums values to them self and pass them to jinja.
     # Thus, we ensure that the output keys in the cloudformation match with the values in class OutputKey
     output_keys_dict = {output_key.value: output_key.value for output_key in OutputKey}
-    return render_template("vm_bucket_cloudformation.jinja.yaml", acl_arn=acl_arn, **output_keys_dict)
+    return render_template("vm_bucket_cloudformation.jinja.yaml", acl_arn=waf_webacl_arn, **output_keys_dict)
 
 
 def _find_vm_bucket_stack_output(aws_access: AwsAccess, output_key: OutputKey):
